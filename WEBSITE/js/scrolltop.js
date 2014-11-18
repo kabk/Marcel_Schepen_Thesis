f26035966d7e89ba05ccc9436444ec3c8318bdf4
Marinus Schepen <mschepen@gmail.com>
2014-11-18 14:16:04 +0100

    All files until 18.11.14
    All files, textual and website

---
+++ WEBSITE/js/scrolltop.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0},2000);
+		return false;
+	});
+	
+	$('a[href^="#"]').on('click', function(event) {
+
+    var target = $( $(this).attr('href') );
+
+    if( target.length ) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+	
+});
